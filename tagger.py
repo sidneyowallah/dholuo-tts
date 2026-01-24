74cf54c1fe0160d0ea9981b88580187e5250cb65
Sidney Owallah <sidneyowallah@gmail.com>
2026-01-23 19:00:10 -0600

    refactor: move DHOLUO_STOPWORDS to tagger.py to fix circular import
    - Move DHOLUO_STOPWORDS dictionary from preprocess_dhonam.py to tagger.py
    - Update preprocess_dhonam.py to import DHOLUO_STOPWORDS from tagger
    - Resolves circular import between tagger.py and preprocess_dhonam.py

--- tagger.py
+++ tagger.py
@@ -1,7 +1,64 @@
 import string
 import torch
 from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
-from preprocess_dhonam import DHOLUO_STOPWORDS
+
+# Dholuo stopwords dictionary
+DHOLUO_STOPWORDS = {
+    # --- PRONOUNS (PRON) ---
+    "an": "PRON", "emane": "PRON", "en": "PRON", "endo": "PRON", "gin": "PRON", 
+    "in": "PRON", "ingi": "PRON", "mekgi": "PRON", "ne": "PRON", "nga": "PRON", 
+    "ngama": "PRON", "ngata": "PRON", "un": "PRON", "wan": "PRON",
+    # --- NOUNS (NN) ---
+    "arus": "NN", "barua": "NN", "betri": "NN", "bote": "NN", "chamcham": "NN", 
+    "chieng": "NN", "chieng'wa": "NN", "dala": "NN", "dend": "NN", "dhano": "NN", 
+    "doo": "NN", "duond": "NN", "duong'": "NN", "dwe": "NN", "gima": "NN", 
+    "gokinyi": "NN", "gweng": "NN", "higa": "NN", "homwake": "NN", "jangad": "NN", 
+    "ji": "NN", "kauntiy": "NN", "kongo": "NN", "kuguok": "NN", "lee": "NN", 
+    "lesonde": "NN", "lo": "NN", "lowo": "NN", "luwor": "NN", "meru": "NN", 
+    "mutasi": "NN", "nam": "NN", "nairobi": "NN", "ndalo": "NN", "ngad": "NN", 
+    "ngano": "NN", "ngat": "NN", "ng'at": "NN", "ngato": "NN", "ng'ato": "NN", 
+    "ng'atno": "NN", "nge": "NN", "ngere": "NN", "nguon": "NN", "nguono": "NN", 
+    "ngut": "NN", "ngwech": "NN", "nyak": "NN", "nyasaye": "NN", "nyathi": "NN", 
+    "nying": "NN", "nyithindo": "NN", "nyoro": "NN", "okek": "NN", "ot": "NN", 
+    "ouma": "NN", "piny": "NN", "protin": "NN", "rangi": "NN", "ranginy": "NN", 
+    "richo": "NN", "sikul": "NN", "taa": "NN", "towel": "NN", "tudruok": "NN", 
+    "wenyiew": "NN", "wiya": "NN", "yat": "NN", "chambwa": "NN",
+    # --- VERBS (V) ---
+    "bedoe": "V", "biro": "V", "chakre": "V", "cham": "V", "chamo": "V", 
+    "chango": "V", "chano": "V", "chiemo": "V", "chiwo": "V", "chulo": "V", 
+    "chuoyo": "V", "dhawo": "V", "dhiye": "V", "dino": "V", "dinuoya": "V", 
+    "dongo": "V", "duoko": "V", "dwaro": "V", "gaye": "V", "gengo": "V", 
+    "godoye": "V", "hero": "V", "hinya": "V", "kayawo": "V", "kendo": "V", 
+    "kwayo": "V", "lam": "V", "lamo": "V", "limo": "V", "loso": "V", 
+    "luok": "V", "luoro": "V", "luongo": "V", "luwo": "V", "miyo": "V", 
+    "mobedo": "V", "modong'": "V", "mogik": "V", "moloyo": "V", "mondiki": "V", 
+    "motegni": "V", "nang'iew": "V", "ndiko": "V", "neno": "V", "ngado": "V", 
+    "nganyo": "V", "nganjo": "V", "ngieyo": "V", "ngiewo": "V", "nginjo": "V", 
+    "ngiyo": "V", "ngol": "V", "ngolo": "V", "ngony": "V", "ngeyo": "V", 
+    "ningogi": "V", "nitie": "V", "noduokie": "V", "nondiko": "V", "nwa": "V", 
+    "nyalo": "V", "nyiewo": "V", "nyisi": "V", "parore": "V", "pidho": "V", 
+    "pog": "V", "pongo": "V", "riembe": "V", "ringo": "V", "riwo": "V", 
+    "romo": "V", "romre": "V", "tayowa": "V", "tedo": "V", "tim": "V", 
+    "timo": "V", "timre": "V", "tingo": "V", "tiyogo": "V", "tony": "V", 
+    "tugo": "V", "wuok": "V", "wuoth": "V", "yie": "V", "yudo": "V", 
+    "yudore": "V",
+    # --- ADJECTIVES (ADJ) ---
+    "mongere": "ADJ", "ngeny": "ADJ", "ngich": "ADJ",
+    # --- ADVERBS (ADV) ---
+    "bange": "ADV", "bangeyo": "ADV", "chon": "ADV", "ka": "ADV", 
+    "kane": "ADV", "kanyiyo": "ADV", "kapok": "ADV", "kariembo": "ADV", 
+    "kinde": "ADV", "machiegni": "ADV", "machon": "ADV", "malo": "ADV", 
+    "nene": "ADV", "ngar": "ADV", "nyime": "ADV",
+    # --- DETERMINERS (DET) ---
+    "duto": "DET", "ma": "DET", "mane": "DET", "moko": "DET", "yodno": "DET",
+    # --- ADPOSITIONS (ADP) ---
+    "e": "ADP", "gi": "ADP", "kaw": "ADP", "kod": "ADP", "kuom": "ADP", 
+    "nyaka": "ADP",
+    # --- CONJUNCTIONS (CONJ) ---
+    "koso": "CONJ", "to": "CONJ",
+    # --- PARTICLES (PART) ---
+    "be": "PART", "bende": "PART", "ni": "PART", "ok": "PART"
+}
 
 class Tagger:
     def __init__(self, model_path="models/luo-pos", base_model="Davlan/afro-xlmr-large-76L"):
